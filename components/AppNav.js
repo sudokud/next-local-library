fe93ce8ad377f5d47e3309335f12171e91ca45c5
okuninoshi <gennesis@outlook.fr>
2021-07-21 16:30:46 +0200

    change navbar postion

--- components/AppNav.js
+++ components/AppNav.js
@@ -1,61 +1,67 @@
 import React from 'react'
 import Link from 'next/link'
-import { Divider } from '@geist-ui/react'
 
 const AppNav = () => {
    return(
-      <aside>
-         <nav>
-            <ul>
-               <li>
-                  <Link href="/catalog">
-                     <a>Home</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/books">
-                     <a>All Books</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/authors">
-                     <a>All Authors</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/genres">
-                     <a>All Genres</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/bookinstances">
-                     <a>All Book Instances</a>
-                  </Link>
-               </li>
-               <Divider />
-               <li >
-                  <Link href="/catalog/authors/create">
-                     <a>Create new author</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/genres/create">
-                     <a>Create new genre</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/books/create">
-                     <a>Create new book</a>
-                  </Link>
-               </li>
-               <li >
-                  <Link href="/catalog/bookinstances/create">
-                     <a>Create new book instance (copy)</a>
-                  </Link>
-               </li>
-            </ul>
-         </nav>
-      </aside>
+      <div style={{
+         position: "fixed",
+         top: 0,
+         left: 0,
+         right: 0,
+         background: "snow"
+      }}>
+      <nav
+         style={{
+            display: "flex",
+            flexDirection:"row",
+            flexWrap:"wrap",
+            justifyContent:"space-between",
+            alignItems:"center",
+            maxWidth:"1000px",
+            margin:"auto",
+            minHeight:"58px",
+            paddingTop:"1.9vh 0",
+            borderBottom:"1px solid lightgrey"
+         }}
+      >
+         <Link href="/catalog">
+            <a>Home</a>
+         </Link>
+      
+         <Link href="/catalog/books">
+            <a>Books</a>
+         </Link>
+      
+         <Link href="/catalog/authors">
+            <a>Authors</a>
+         </Link>
+      
+         <Link href="/catalog/genres">
+            <a>Genres</a>
+         </Link>
+      
+         <Link href="/catalog/bookinstances">
+            <a>Book Instances</a>
+         </Link>
+      
+      
+         <Link href="/catalog/authors/create">
+            <a>+ author</a>
+         </Link>
+      
+         <Link href="/catalog/genres/create">
+            <a>+ genre</a>
+         </Link>
+   
+         <Link href="/catalog/books/create">
+            <a>+ book</a>
+         </Link>
+      
+         <Link href="/catalog/bookinstances/create">
+            <a>+ bookinstance</a>
+         </Link>
+      </nav>
+      </div>
    )
 }
 
