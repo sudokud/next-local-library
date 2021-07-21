b678a900218e5a32774a9291bf714f7e2942e321
okuninoshi <gennesis@outlook.fr>
2021-07-21 16:28:31 +0200

    add comment

--- utils/Fetcher.js
+++ utils/Fetcher.js
@@ -1,5 +1,5 @@
+// visit https://swr.vercel.app/docs/with-nextjs to know more about SWR and the Fetcher function
 export default async function Fetcher(...args) {
-   const res = await fetch(...args);
- 
-   return res.json();
- }
+  const res = await fetch(...args);
+  return res.json();
+}
